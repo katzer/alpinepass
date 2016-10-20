f345c14e4c1dc81dfcbb672b86d9d9a376a7b208
Stefan Härtel <haertel.dev@gmail.com>
2016-10-20 09:37:19 +0200

    Read the file

--- alpinepass.go
+++ alpinepass.go
@@ -2,9 +2,16 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	//"github.com/go-yaml/yaml"
 )
 
 func main() {
-	fmt.Printf("Hello, world!\n")
+	fmt.Printf("Readin input.yaml!\n")
+
+	data, err := ioutil.ReadFile("input.yaml")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Print(string(data))
 }
