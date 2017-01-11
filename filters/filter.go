e812c56f21fc20d2165e1131a65b151f73147c7d
Stefan Härtel <haertel.dev@gmail.com>
2017-01-11 10:06:39 +0100

    Implement and use passwordfilter

--- filters/filter.go
+++ filters/filter.go
@@ -10,9 +10,10 @@ type Filter interface {
 }
 
 func FilterConfigs(configs []d.Config, context *cli.Context) []d.Config {
+	passwordFilter := PasswordFilter{}
 	if !context.GlobalBool("passwords") {
 		for i := 0; i < len(configs); i++ {
-			configs[i].Password = ""
+			configs[i] = passwordFilter.filter(configs[i])
 		}
 	}
 	return configs
--- filters/passwordfilter.go
+++ filters/passwordfilter.go
@@ -1,4 +1,12 @@
 package filters
 
+import d "github.com/appPlant/alpinepass/data"
+
+// PasswordFilter removes passwords from a Config.
 type PasswordFilter struct {
+}
+
+func (p PasswordFilter) filter(data d.Config) d.Config {
+	data.Password = ""
+	return data
 }
