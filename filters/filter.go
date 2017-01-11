3346df3a57a64527dcdbd701c4f8a282155ecd3c
Stefan Härtel <haertel.dev@gmail.com>
2017-01-11 09:42:40 +0100

    Move filterConfigs function to filters package

--- filters/filter.go
+++ filters/filter.go
@@ -1,5 +1,19 @@
 package filters
 
+import (
+	d "github.com/appPlant/alpinepass/data"
+	"github.com/urfave/cli"
+)
+
 type Filter interface {
-	filter()
+	filter(data d.Config) d.Config
+}
+
+func filterConfigs(configs []d.Config, context *cli.Context) []d.Config {
+	if !context.GlobalBool("passwords") {
+		for i := 0; i < len(configs); i++ {
+			configs[i].Password = ""
+		}
+	}
+	return configs
 }
