312f9555b20771b67b8ed1d4438d06b95f384d25
Stefan Härtel <haertel.dev@gmail.com>
2017-02-01 18:46:43 +0100

    Create new „app“ package

---
+++ app/commands.go
@@ -0,0 +1,73 @@
+package app
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/urfave/cli"
+	yaml "gopkg.in/yaml.v2"
+
+	d "github.com/appPlant/alpinepass/data"
+	"github.com/appPlant/alpinepass/filters"
+	"github.com/appPlant/alpinepass/io"
+	"github.com/appPlant/alpinepass/util"
+)
+
+// Separator separates the different parts of an ID
+const Separator string = "."
+
+func parseData(data string) d.YamlData {
+	yamlData := d.YamlData{}
+	err := yaml.Unmarshal([]byte(data), &yamlData)
+	util.CheckError(err)
+	return yamlData
+}
+
+func createConfig(definition d.Definition) d.Config {
+	config := d.Config{}
+	config.Title = definition.Title
+	config.ID = createID(definition)
+	config.Password = definition.Password
+	config.User = definition.User
+	//TODO host
+	return config
+}
+
+func createID(definition d.Definition) string {
+	id := ""
+	id = id + util.CleanString(definition.Location) + Separator
+	id = id + util.CleanString(definition.Env) + Separator
+	id = id + util.CleanString(definition.Type) + Separator
+	id = id + util.CleanString(definition.Title)
+	return id
+}
+
+func readConfigs() []d.Config {
+	data := io.ReadFile()
+	yamlData := parseData(data)
+	configs := []d.Config{}
+
+	for _, definition := range yamlData.Defs {
+		config := createConfig(definition)
+		configs = append(configs, config)
+	}
+
+	return configs
+}
+
+func RunShowCommand(context *cli.Context) error {
+	configs := readConfigs()
+	configs = filters.FilterConfigs(configs, context)
+	configsJSON, err := json.MarshalIndent(configs, "", "    ")
+	util.CheckError(err)
+	fmt.Println(string(configsJSON))
+
+	return nil
+}
+
+func RunOutCommand(context *cli.Context) error {
+	configs := readConfigs()
+	configs = filters.FilterConfigs(configs, context)
+	io.WriteJSON(configs)
+	return nil
+}
