aa42102891a8846a85e0b1051eb72792a80fbc37
Stefan Härtel <haertel.dev@gmail.com>
2017-02-07 16:24:56 +0100

    Adjust input file

--- filters/propertyfilter.go
+++ filters/propertyfilter.go
@@ -1,19 +1,14 @@
 package filters
 
-import (
-	"fmt"
-
-	d "github.com/appPlant/alpinepass/data"
-)
+import d "github.com/appPlant/alpinepass/data"
 
 //PropertyFilter allows filtering a Config for the content of its properties.
 type PropertyFilter struct {
 	Slices []string
 }
 
-func (n PropertyFilter) filter(data d.Config) d.Config {
+func (p PropertyFilter) filter(data d.Config) d.Config {
 	//var remove = false
-	fmt.Println(n.Slices)
 	return data
 }
 
