6514a2933eabceac7f3703d016bc8324c5c1214e
Stefan Härtel <haertel.dev@gmail.com>
2017-02-07 17:21:55 +0100

    Read the filter flags

--- filters/propertyfilter.go
+++ filters/propertyfilter.go
@@ -1,6 +1,8 @@
 package filters
 
 import d "github.com/appPlant/alpinepass/data"
+import "strings"
+import "fmt"
 
 //PropertyFilter allows filtering a Config for the content of its properties.
 type PropertyFilter struct {
@@ -10,12 +12,17 @@ type PropertyFilter struct {
 func (p PropertyFilter) filter(data d.Config) d.Config {
 	verifyFlags(p.Slices) //TODO verify only once and not for every Config
 	//var remove = false
-
+	for i := 0; i < len(p.Slices); i++ {
+		filterProperty(p.Slices[i], data)
+	}
 	return data
 }
 
-func filterProperty() {
-
+func filterProperty(property string, data d.Config) {
+	var split = strings.Split(property, ":")
+	var key = split[0]
+	var value = split[1]
+	fmt.Println(key + " " + value)
 }
 
 func verifyFlags(flags []string) {
