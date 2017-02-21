c344f2803407ce0e917d52eb7ef551064c2fc518
Stefan Härtel <haertel.dev@gmail.com>
2017-02-21 10:09:24 +0100

    Add validation package

--- app/execution.go
+++ app/execution.go
@@ -9,12 +9,15 @@ import (
 	"github.com/appPlant/alpinepass/filters"
 	"github.com/appPlant/alpinepass/io"
 	"github.com/appPlant/alpinepass/util"
+	"github.com/appPlant/alpinepass/validation"
 )
 
 //execute reads the input, filters it and writes the output.
 func execute(context *cli.Context) error {
 	configs := io.ReadConfigs(context.GlobalString("input"))
 	configs = filters.FilterConfigs(configs, context)
+
+	validation.Validate(configs)
 
 	if context.GlobalBool("display") {
 		var configsJSON []byte
@@ -27,7 +30,7 @@ func execute(context *cli.Context) error {
 		util.CheckError(err)
 		fmt.Println(string(configsJSON))
 	} else {
-		io.WriteJSON(context.GlobalString("output"), configs, context.GlobalBool("pretty"))
+		io.WriteJSON(context.GlobalString("output"), configs, context.GlobalBool("readable"))
 	}
 
 	return nil
---
+++ validation/validation.go
@@ -0,0 +1,9 @@
+package validation
+
+import (
+	"github.com/appPlant/alpinepass/data"
+)
+
+func Validate(configs []data.Config) {
+
+}
