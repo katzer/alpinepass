b741f02ff88039f11b1df0b89df75c4fabcb0f79
Stefan Härtel <haertel.dev@gmail.com>
2017-03-03 08:17:38 +0100

    remove obsolete file, add new bintest script

--- bintest/alpinepass.rb
+++
@@ -1,9 +0,0 @@
-require 'open3'
-require 'test/unit'
-
-BIN  = ARGV.fetch(0).freeze
-PATH = { 'PATH' => "#{File.expand_path('tools', __dir__)}:#{ENV['PATH']}"  }
-
-class TestAlpinepass < Test::Unit::TestCase
-	def test_run
-	end
