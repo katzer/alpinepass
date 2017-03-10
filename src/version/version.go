218123c79222bf2ead68ec7aca98cbb3ea341f58
Stefan Härtel <haertel.dev@gmail.com>
2017-03-10 09:43:59 +0100

    Adjust version program

--- src/version/version.go
+++ src/version/version.go
@@ -3,9 +3,9 @@ package main
 import (
 	"os"
 
-	//"github.com/appPlant/alpinepass/src/util"
+	"github.com/appPlant/alpinepass/src/util"
 )
 
 func main() {
-	os.Stdout.WriteString("foobar")
+	os.Stdout.WriteString(util.Version)
 }
