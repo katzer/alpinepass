2ae35e6722b8ef5c00056ccb5e0696b75c3742cd
Stefan Härtel <haertel.dev@gmail.com>
2017-03-10 14:04:01 +0100

    Revert some build scripts

--- build_config.glibc-2.12.rb
+++ build_config.glibc-2.12.rb
@@ -5,13 +5,13 @@ require 'go/build'
 Go::Build.new('x86_64-pc-linux-gnu-glibc-2.12') do
   os :linux
   arch :amd64
-  appname :alpinepass
+  appname :ski
   bintest_if OS.linux? && OS.bits == 64
 end
 
 Go::Build.new('i686-pc-linux-gnu-glibc-2.12') do
   os :linux
   arch :'386'
-  appname :alpinepass
+  appname :ski
   bintest_if OS.linux? && OS.bits == 32
 end
--- build_config.glibc-2.14.rb
+++ build_config.glibc-2.14.rb
@@ -5,6 +5,6 @@ require 'go/build'
 Go::Build.new('x86_64-pc-linux-gnu') do
   os :linux
   arch :amd64
-  appname :alpinepass
+  appname :ski
   bintest_if OS.linux? && OS.bits == 64
 end
