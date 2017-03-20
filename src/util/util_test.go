0b713e5c48c6a73ae92ed6c36fa2183d19a3c45d
Stefan Härtel <haertel.dev@gmail.com>
2017-03-20 09:38:41 +0100

    Use assert library for testing

--- src/util/util_test.go
+++ src/util/util_test.go
@@ -1,17 +1,14 @@
 package util
 
 import "testing"
+import "github.com/stretchr/testify/assert"
 
 func TestCleanStringFiller(t *testing.T) {
-	if Filler != "_" {
-		t.Errorf("The Filler string might not match the other tests in this file.")
-	}
+	assert.Equal(t, "_", Filler, "The Filler string might not match the other tests in this file.")
 }
 
 func TestCleanString(t *testing.T) {
 	expected := "This_is_some_input."
 	actual := CleanString("This is some input.")
-	if expected != actual {
-		t.Errorf("The cleaned strings do not match.")
-	}
+	assert.Equal(t, expected, actual, "The cleaned strings should match.")
 }
