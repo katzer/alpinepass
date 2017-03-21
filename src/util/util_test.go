67fcd1e4153ab193f27e7f0bcd86314a13d41bf6
Stefan Härtel <haertel.dev@gmail.com>
2017-03-21 09:46:26 +0100

    Add unittest

--- src/util/util_test.go
+++ src/util/util_test.go
@@ -4,11 +4,21 @@ import "testing"
 import "github.com/stretchr/testify/assert"
 
 func TestCleanStringFiller(t *testing.T) {
-	assert.Equal(t, "_", Filler, "The Filler string might not match the other tests in this file.")
+	assert.Equal(t, "_", Filler,
+		"The Filler string might not match the other tests in this file.")
 }
 
 func TestCleanString(t *testing.T) {
 	expected := "This_is_some_input."
 	actual := CleanString("This is some input.")
-	assert.Equal(t, expected, actual, "The cleaned strings should match.")
+
+	assert.Equal(t, expected, actual,
+		"The cleaned strings should match.")
+}
+
+func TestStringInArray(t *testing.T) {
+	assert.Equal(t, StringInArray("bar", []string{"foo", "bar", "baz"}), true,
+		"The list should contain the item.")
+	assert.Equal(t, StringInArray("boo", []string{"foo", "bar", "baz"}), false,
+		"The list should not contain the item.")
 }
