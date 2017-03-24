3870221311c24130c87476cb14b9670cc56f329e
Stefan Härtel <haertel.dev@gmail.com>
2017-03-24 08:38:05 +0100

    Adjust test

--- src/filters/propertyfilter_test.go
+++ src/filters/propertyfilter_test.go
@@ -5,15 +5,19 @@ import "github.com/stretchr/testify/assert"
 import "github.com/appPlant/alpinepass/src/data"
 
 func TestFilterLocationTrue(t *testing.T) {
-	config := data.Config{Location: "Earth"}
 	filter := "location=Earth"
-	assert.Equal(t, true, filterProperty(filter, config).IsValid,
+	config := data.Config{Location: "Earth"}
+	filteredConfig := filterProperty(filter, config)
+
+	assert.Equal(t, true, filteredConfig.IsValid,
 		"The Config should match the filter.")
 }
 
 func TestFilterLocationFalse(t *testing.T) {
-	config := data.Config{Location: "Moon"}
 	filter := "location=Earth"
-	assert.Equal(t, false, filterProperty(filter, config).IsValid,
+	config := data.Config{Location: "Moon"}
+	filteredConfig := filterProperty(filter, config)
+
+	assert.Equal(t, false, filteredConfig.IsValid,
 		"The Config should not match the filter.")
 }
