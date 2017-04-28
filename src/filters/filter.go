faa3d3f00c7634e0689fb45c1e22a7026fde79c9
Stefan Härtel <haertel.dev@gmail.com>
2017-04-28 09:16:23 +0200

    Remove package alias

--- src/filters/filter.go
+++ src/filters/filter.go
@@ -1,17 +1,17 @@
 package filters
 
 import (
-	d "github.com/appPlant/alpinepass/src/data"
+	"github.com/appPlant/alpinepass/src/data"
 	"github.com/urfave/cli"
 )
 
 //Filter declares all methods used for implementing filters.
 type Filter interface {
-	filter(data d.Config) d.Config
+	filter(config data.Config) data.Config
 }
 
 //FilterConfigs applies all filters to each Config.
-func FilterConfigs(configs []d.Config, context *cli.Context) []d.Config {
+func FilterConfigs(configs []data.Config, context *cli.Context) []data.Config {
 	if !context.GlobalBool("passwords") {
 		passwordFilter := PasswordFilter{}
 		configs = applyFilter(configs, passwordFilter)
@@ -23,8 +23,8 @@ func FilterConfigs(configs []d.Config, context *cli.Context) []d.Config {
 	return configs
 }
 
-func applyFilter(configs []d.Config, filter Filter) []d.Config {
-	cleanedConfigs := []d.Config{}
+func applyFilter(configs []data.Config, filter Filter) []data.Config {
+	cleanedConfigs := []data.Config{}
 
 	for i := 0; i < len(configs); i++ {
 		configs[i] = filter.filter(configs[i])
