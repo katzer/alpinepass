54d7f30bc3cb871cfa17f8e52c841b129617403b
Stefan Härtel <haertel.dev@gmail.com>
2017-04-28 11:04:05 +0200

    Remove package alias

--- src/filters/passwordfilter.go
+++ src/filters/passwordfilter.go
@@ -1,13 +1,13 @@
 package filters
 
-import d "github.com/appPlant/alpinepass/src/data"
+import "github.com/appPlant/alpinepass/src/data"
 
 // PasswordFilter removes passwords from a Config.
 type PasswordFilter struct {
 	strings []string
 }
 
-func (p PasswordFilter) filter(data d.Config) d.Config {
-	data.Password = ""
-	return data
+func (p PasswordFilter) filter(config data.Config) data.Config {
+	config.Password = ""
+	return config
 }
