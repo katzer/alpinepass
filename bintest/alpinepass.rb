844dcc6d158fba60e060750e23fbb662de2d0937
Stefan Härtel <haertel.dev@gmail.com>
2017-05-12 09:30:42 +0200

    Remove obsolete bintest, prepare new bintest

--- bintest/alpinepass.rb
+++ bintest/alpinepass.rb
@@ -6,29 +6,23 @@ PATH = { 'PATH' => "#{File.expand_path('tools', __dir__)}:#{ENV['PATH']}"  }
 LOCATION = BIN.gsub('alpinepass', '') #The path used for input and output files.
 
 class TestAlpinepass < Test::Unit::TestCase
-	def test_run
-		output, error, status = Open3.capture3(PATH, BIN)
-
-    puts output
-    puts "### STATUS: "
-    puts status
-    puts status.success?
-		assert_false status.success?, 'Process should fail!'
-		expect_error(output, error, 'Run the application.')
-    assert_include output, 'The file input.yml does not exist!', 'The error message is not correct!'
-	end
-
-  def test_run_input_valid
-		output, error, status = Open3.capture3(PATH, BIN, "-i=#{LOCATION}valid.yml")
-		assert_true status.success?, 'Process did not exit cleanly!'
-		expect_no_error(output, error, 'Use valid.yml.')
-	end
-
   def test_show_the_version
 		output, error, status = Open3.capture3(PATH, BIN, '-v')
 		assert_true status.success?, 'Process did not exit cleanly!'
 		expect_no_error(output, error, 'Show the version.')
     assert_include output, 'alpinepass version'
+	end
+
+  #def test_run_input_invalid
+	#	output, error, status = Open3.capture3(PATH, BIN, "-i=#{LOCATION}invalid.yml")
+	#	assert_false status.success?, 'Process should fail!'
+	#	expect_error(output, error, 'Use invalid.yml. There should be a validation error.')
+	#end
+
+  def test_run_input_valid
+		output, error, status = Open3.capture3(PATH, BIN, "-i=#{LOCATION}valid.yml")
+		assert_true status.success?, 'Process did not exit cleanly!'
+		expect_no_error(output, error, 'Use valid.yml.')
 	end
 end
 
