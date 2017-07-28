6d0666dbddb3a72914ca04473773411cc26b925e
Stefan Härtel <haertel.dev@gmail.com>
2017-07-28 09:11:13 +0200

    Move flag helper to separate file

--- src/app/application.go
+++ src/app/application.go
@@ -7,30 +7,6 @@ import (
 	"github.com/urfave/cli"
 )
 
-//FlagDebug holds the name for the debug flag.
-const FlagDebug = "debug"
-
-//FlagDisplay holds the name for the display flag.
-const FlagDisplay = "display"
-
-//FlagFilter holds the name for the filter flag.
-const FlagFilter = "filter"
-
-//FlagInput holds the name for the input flag.
-const FlagInput = "input"
-
-//FlagOutput holds the name for the output flag.
-const FlagOutput = "output"
-
-//FlagPasswords holds the name wor the passwords flag.
-const FlagPasswords = "passwords"
-
-//FlagReadable holds the name for the readable flag.
-const FlagReadable = "readable"
-
-//FlagSkip holds the name for the skip flag.
-const FlagSkip = "skip"
-
 //RunApp sets up the cli application and executes it.
 func RunApp() {
 	app := cli.NewApp()
---
+++ src/app/flags.go
@@ -0,0 +1,25 @@
+package app
+
+//FlagDebug holds the name for the debug flag.
+const FlagDebug = "debug"
+
+//FlagDisplay holds the name for the display flag.
+const FlagDisplay = "display"
+
+//FlagFilter holds the name for the filter flag.
+const FlagFilter = "filter"
+
+//FlagInput holds the name for the input flag.
+const FlagInput = "input"
+
+//FlagOutput holds the name for the output flag.
+const FlagOutput = "output"
+
+//FlagPasswords holds the name wor the passwords flag.
+const FlagPasswords = "passwords"
+
+//FlagReadable holds the name for the readable flag.
+const FlagReadable = "readable"
+
+//FlagSkip holds the name for the skip flag.
+const FlagSkip = "skip"
