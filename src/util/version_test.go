5963401d4815acb728082a91b14baacd1b0b89bd
Stefan Härtel <haertel.dev@gmail.com>
2017-09-01 11:26:05 +0200

    Add sanity test

---
+++ src/util/version_test.go
@@ -0,0 +1,9 @@
+package util
+
+import "testing"
+import "github.com/stretchr/testify/assert"
+
+func TestVersionIsCorrect(t *testing.T) {
+	assert.Equal(t, "1.4.2", Version,
+		"The version is not correct. Did you forget it to adjust somewhere?")
+}
