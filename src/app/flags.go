1cb45b4d9e9c360e34cc6042bc2a04f046519412
Stefan Härtel <haertel.dev@gmail.com>
2017-09-08 09:18:18 +0200

    Formatting

--- src/app/flags.go
+++ src/app/flags.go
@@ -1,25 +1,25 @@
 package app
 
-//FlagDebug holds the name for the debug flag.
+//FlagDebug holds the name for the "debug" flag.
 const FlagDebug = "debug"
 
-//FlagDisplay holds the name for the display flag.
+//FlagDisplay holds the name for the "display" flag.
 const FlagDisplay = "display"
 
-//FlagFilter holds the name for the filter flag.
+//FlagFilter holds the name for the "filter" flag.
 const FlagFilter = "filter"
 
-//FlagInput holds the name for the input flag.
+//FlagInput holds the name for the "input" flag.
 const FlagInput = "input"
 
-//FlagOutput holds the name for the output flag.
+//FlagOutput holds the name for the "output" flag.
 const FlagOutput = "output"
 
-//FlagPasswords holds the name wor the passwords flag.
+//FlagPasswords holds the name wor the "passwords" flag.
 const FlagPasswords = "passwords"
 
-//FlagReadable holds the name for the readable flag.
+//FlagReadable holds the name for the "readable" flag.
 const FlagReadable = "readable"
 
-//FlagSkip holds the name for the skip flag.
+//FlagSkip holds the name for the "skip" flag.
 const FlagSkip = "skip"
