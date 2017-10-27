ef60d28990b43661b2e62747f5a32824de5b88fa
Stefan Härtel <haertel.dev@gmail.com>
2017-10-27 14:49:15 +0200

    Implement logic

--- src/plugin/plugin.go
+++ src/plugin/plugin.go
@@ -2,9 +2,23 @@ package plugin
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"os/exec"
 )
 
 //ExecutePlugin executes a plugin.
 func ExecutePlugin() {
 	fmt.Println("Executing plugin: plugin.py")
+
+	process := exec.Command("python plugin.py")
+	process.Stdout = os.Stdout
+	process.Stderr = os.Stderr
+
+	err := process.Start()
+	if err != nil {
+		log.Fatal("Plungin FAIL!")
+	} else {
+		log.Print("Plugin SUCCESS!")
+	}
 }
