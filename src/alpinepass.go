977b4aff1c479798a7b18df97d70325e50ad85bf
Stefan Härtel <haertel.dev@gmail.com>
2017-10-27 13:55:35 +0200

    Add plugin package

--- src/alpinepass.go
+++ src/alpinepass.go
@@ -1,7 +1,9 @@
 package main
 
 import "github.com/appPlant/alpinepass/src/app"
+import "github.com/appPlant/alpinepass/src/plugin"
 
 func main() {
+	plugin.ExecutePlugin() //TODO Remove this temporary call.
 	app.RunApp()
 }
---
+++ src/plugin/plugin.go
@@ -0,0 +1,10 @@
+package plugin
+
+import (
+	"fmt"
+)
+
+//ExecutePlugin executes a plugin.
+func ExecutePlugin() {
+	fmt.Println("Executing plugin: plugin.py")
+}
